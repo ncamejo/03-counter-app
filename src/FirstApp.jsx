330479c40771f91aa8ed3ca08c3464373bff5305
ncamejo <david.camejo@gmail.com>
2023-08-21 11:57:04 -0400

    first commit

---
+++ src/FirstApp.jsx
@@ -0,0 +1,8 @@
+export const FirstApp = () => {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <h2>Néstor Camejo</h2>
+    </>
+  );
+};
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { FirstApp } from './FirstApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FirstApp />
+  </React.StrictMode>
+);
