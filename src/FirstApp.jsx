43642894f896151301f3cbf54f2297adfa827fe1
ncamejo <david.camejo@gmail.com>
2023-08-24 16:24:46 -0400

    section

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,16 +1,23 @@
 import PropTypes from 'prop-types'
 
-export const FirstApp = ({title, subTitle}) => {
+export const FirstApp = ({title, subTitle="La tierra explota", name}) => {
 
   return (
     <>
     <h1>{title}</h1>
       <p>{subTitle}</p>
+      <p>{name}</p>
     </>
   );
 };
 
 FirstApp.propTypes = {
   title: PropTypes.string.isRequired,
-  subTitle: PropTypes.number.isRequired
+  subTitle: PropTypes.number
+}
+
+FirstApp.defaultProps = {
+  title : 'No hay titulo',
+  subTitle : 'No hay subtitulo',
+  name: 'Néstor Camejo'
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp title="Hola, soy Goku" subTitle={123}/>
+    <FirstApp title="Hola, soy Goku"/>
   </React.StrictMode>
 );
