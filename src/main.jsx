1251cff3c52d57093354650b26487a7de697ee05
Néstor <david.camejo@gmail.com>
2023-08-27 13:44:26 -0400

    fin de la sección 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FirstApp } from './FirstApp';
+// import { FirstApp } from './FirstApp';
 import './styles.css';
+import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp title="Hola, soy Goku"/>
+    <CounterApp value={20}/>
   </React.StrictMode>
 );
